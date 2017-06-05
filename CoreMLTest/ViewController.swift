7c8a3da6e94894def4f8cae3e9734164c8026597
Josh Smith <josh.smith@bottlerocketapps.com>
2017-06-05 16:40:58 -0500

    Initial Commit

---
+++ CoreMLTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLTest
+//
+//  Created by Josh Smith  on 6/5/17.
+//  Copyright © 2017 BottleRocketStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
