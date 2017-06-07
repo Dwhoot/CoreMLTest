dad96802aee3802d1c46c9672a6761ee5c1a4ca1
Josh Smith <josh.smith@bottlerocketapps.com>
2017-06-07 17:53:07 -0500

    Actually upload the code.

--- CoreMLTest/ViewController.swift
+++ CoreMLTest/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+	let model = GoogLeNetPlaces()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		let first = #imageLiteral(resourceName: "image3")
+		let second = #imageLiteral(resourceName: "image2")
+		let third = #imageLiteral(resourceName: "image3")
+		let fourth = #imageLiteral(resourceName: "image4")
+		let imageArray = [first, second, third, fourth]
+		
+		for image in imageArray {
+			guard let firstPixelBuffer = CreatePixelBufferFromImage(image) else { fatalError("Creating Pixel buffer failed.") }
+			guard let firstResult = try? model.prediction(sceneImage: firstPixelBuffer) else { fatalError("Unexpected runtime error.") }
+			print(firstResult.sceneLabel)
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	fileprivate func CreatePixelBufferFromImage(_ image: UIImage) -> CVPixelBuffer?{
+		let size = image.size
+		var pxbuffer : CVPixelBuffer?
+		let pixelBufferPool = createPixelBufferPool(224, 224, FourCharCode(kCVPixelFormatType_32BGRA), 2056) // Hard coded values for demo purposes.
+		let status = CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, pixelBufferPool!, &pxbuffer)
+		
+		guard (status == kCVReturnSuccess) else{
+			return nil
+		}
+		
+		CVPixelBufferLockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0))
+		let pxdata = CVPixelBufferGetBaseAddress(pxbuffer!)
+		let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+		let context = CGContext(data: pxdata,
+		                        width: Int(size.width),
+		                        height: Int(size.height),
+		                        bitsPerComponent: 8,
+		                        bytesPerRow: CVPixelBufferGetBytesPerRow(pxbuffer!),
+		                        space: rgbColorSpace,
+		                        bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+		
+		context?.translateBy(x: 0, y: image.size.height)
+		context?.scaleBy(x: 1.0, y: -1.0)
+		UIGraphicsPushContext(context!)
+		image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+		UIGraphicsPopContext()
+		CVPixelBufferUnlockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0))
+		return pxbuffer
 	}
-
-
+	
+	fileprivate func createPixelBufferPool(_ width: Int32, _ height: Int32, _ pixelFormat: FourCharCode, _ maxBufferCount: Int32) -> CVPixelBufferPool? {
+		var outputPool: CVPixelBufferPool? = nil
+		let sourcePixelBufferOptions: NSDictionary = [kCVPixelBufferPixelFormatTypeKey: pixelFormat,
+		                                              kCVPixelBufferWidthKey: width,
+		                                              kCVPixelBufferHeightKey: height,
+		                                              kCVPixelFormatOpenGLESCompatibility: true,
+		                                              kCVPixelBufferIOSurfacePropertiesKey: NSDictionary()]
+		
+		let pixelBufferPoolOptions: NSDictionary = [kCVPixelBufferPoolMinimumBufferCountKey: maxBufferCount]
+		CVPixelBufferPoolCreate(kCFAllocatorDefault, pixelBufferPoolOptions, sourcePixelBufferOptions, &outputPool)
+		return outputPool
+	}
 }
 
